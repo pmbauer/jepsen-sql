8bf94f46a16f616e7dcfe3ee01d139d7aac63e49
Paul Bauer <paul.bauer@supabase.io>
2026-07-29 18:57:18 -0400

    add antithesis dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.5" :scope "provided"]
-                 [jepsen "0.3.12-SNAPSHOT"]
+                 [jepsen "0.3.12"]
+                 [io.jepsen/antithesis "0.1.0"]
                  [com.github.seancorfield/next.jdbc "1.3.1093"]
                  [com.gfredericks/test.chuck "0.2.15"]
                  [org.clojure/test.check "1.1.3"]]
